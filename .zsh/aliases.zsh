7fe9ff7bd339da61db69bb37b277e1fc864b553b
Lei Cen <cenlei2000@gmail.com>
2015-04-24 17:24:51 -0400

    rm to trash, remove to /bin/rm

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,7 +2,7 @@ alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 alias rcd='ranger-cd'
-alias 'bk=cd $OLDPWD'
+alias bk='cd $OLDPWD'
 alias by='byobu-tmux'
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
@@ -30,25 +30,17 @@ alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo
 alias =clear
 alias matlabno="matlab -nodesktop -nosplash"
 alias python="ipython"
-alias shortcut='gnome-desktop-item-edit ~/Desktop/ --create-new'
+# alias shortcut='gnome-desktop-item-edit ~/Desktop/ --create-new'
 # alias tmux="TERM=screen-256color-bce tmux -2"
 alias tm="tmux -2 -u"
 alias howdoi="howdoi -c -n 3"
 alias -g L="|less" # Write L after a command to page through the output.
-alias -g NUL="> /dev/null 2>&1" # You get the idea.
+alias -g NULL="> /dev/null 2>&1" # You get the idea.
 alias -s py=vim
 alias -s m=vim
 alias -s txt=vim
 alias -s log=vim
 alias v='f -e vim'
-alias a='fasd -a'        # any
-alias s='fasd -si'       # show / search / select
-alias d='fasd -d'        # directory
-alias f='fasd -f'        # file
-alias sd='fasd -sid'     # interactive directory selection
-alias sf='fasd -sif'     # interactive file selection
-alias z='fasd_cd -d'     # cd, same functionality as j in autojump
-alias zz='fasd_cd -d -i'
 # servers
 alias chicago="ssh lcen@chicago.cs.purdue.edu -t 'cd /scratch7/lcen;zsh'"
 alias km="ssh lcen@kunming.cs.purdue.edu -t 'cd /scratch7/lcen;zsh'"
@@ -56,3 +48,6 @@ alias nb="ssh lcen@ningbo.cs.purdue.edu -t 'cd /scratch7/lcen;zsh'"
 alias sj="ssh lcen@sanjose.cs.purdue.edu -t 'cd /scratch7/lcen;zsh'"
 alias mc18="ssh lcen@mc18.cs.purdue.edu -t 'zsh'"
 
+# trash
+alias rm=trash
+alias remove='/bin/rm'
